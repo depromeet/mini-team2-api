cc083dba1513395f9f3af7613ef05b1c40f19323
Haeseong Jeon <seong0428@gmail.com>
2018-04-09 00:04:18 +0900

    Create files
    - create a service package
    - create a mapper package
    - create a model package
    - create a dto package
    - add a lombok

--- src/main/java/com/depromeet/mini/team2/api/controller/BoardController.java
+++ src/main/java/com/depromeet/mini/team2/api/controller/BoardController.java
@@ -1,15 +1,22 @@
 package com.depromeet.mini.team2.api.controller;
 
-import java.util.Collections;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.depromeet.mini.team2.api.dto.BoardDTO;
+import com.depromeet.mini.team2.api.service.BoardService;
+
 @RestController
 public class BoardController {
+	@Autowired
+	private BoardService boardService;
 
 	@RequestMapping(value = "/api/boards", method = RequestMethod.POST)
 	public String createBoard() {
@@ -19,10 +26,16 @@ public class BoardController {
 	}
 
 	@RequestMapping(value = "/api/boards/{boardId}", method = RequestMethod.GET)
-	public List<String> getBoards(@PathVariable int boardId) {
+	public BoardDTO getBoard(@PathVariable int boardId) {
+		BoardDTO baordDTO = boardService.getBoard(boardId);
 		
-		
-		return Collections.emptyList();
+		return baordDTO;
 	}
 
+	@RequestMapping(value = "/api/boards", method = RequestMethod.GET)
+	public List<BoardDTO> getBoards() {
+		
+		
+		return boardService.getBoards();
+	}
 }
